2aa38c84447751d7b2145f60848350c67e94d172
JenCW <Jencortezwalters@Gmail.com>
2025-09-05 12:39:15 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
-module.exports = nextConfig
+  // Add custom Next.js config options here if needed
+};
 
+module.exports = nextConfig;
