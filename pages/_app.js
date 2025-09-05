ac2c5c5bfbeccdb0279a7bc8f7707fedbfa2c7ba
JenCW <Jencortezwalters@Gmail.com>
2025-09-05 16:04:25 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
