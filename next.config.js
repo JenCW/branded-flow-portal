94b173b287a796db7e155a8331a0969625c7b88b
JenCW <Jencortezwalters@Gmail.com>
2025-09-05 15:38:12 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig}  */
-const nextConfig = {
-  reactStrictMode: true,
-  // keep it simple; add options later if you actually need them
-};
-
-module.exports = nextConfig;
-
