a775c079a436531eb0c511782044a0ba928a67b2
JenCW <Jencortezwalters@Gmail.com>
2025-09-06 00:15:13 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import Portal from '../components/Portal'
 
+// Check if environment variables are available
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error('Missing NEXT_PUBLIC_SUPABASE_URL')
+}
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY')
+}
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
